3f5a7ef4d20ed38747925d3aff458e38a00b718e
KF <keithfry@gmail.com>
2023-02-05 20:24:42 -0500

    Made simple rest controller , get request

--- web-app-embedded/src/main/java/com/example/webappembedded/WebAppEmbeddedApplication.java
+++ web-app-embedded/src/main/java/com/example/webappembedded/WebAppEmbeddedApplication.java
@@ -1,20 +1,20 @@
 package com.example.webappembedded;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class WebAppEmbeddedApplication extends SpringBootServletInitializer {
+public class WebAppEmbeddedApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppEmbeddedApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return super.configure(builder);
 	}
 
 }
